46cbe927a1c272624791d2d3bbc1a2c36be8e5ef
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-14 18:53:18 +0900

    adds components folder with About, Bonsai, Home, Shodo, Navigation js files and builds Routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Bonsai from './components/About';
+import Home from './components/Home';
+import Navigation from './components/Navigation';
+import Bonsai from './components/Bonsai';
+import Shodo from './components/Shodo';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <main>
+          <Routes>
+            <Route path ='/about' element={<About />}/>
+            <Route path='/home' element={<CarouselContainer data={data} />}/>
+            <Route path='*' element={<Navigate to ='/home' />}/>
+        </Routes>
+      </main>
+    </>
   );
 }
 
