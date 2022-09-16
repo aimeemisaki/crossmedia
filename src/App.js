ee239c0b7c7cd98e94fff1acac274410cdfebaa8
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-16 13:44:49 +0900

    adds Contact.js to routes in App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About'
 import Navigation from './components/Navigation';
 import Bonsai from './components/Bonsai';
 import Shodo from './components/Shodo';
+import Contact from './components/Contact'
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path ='/about' element={<About />}/>
             <Route path='/bonsai' element={<Bonsai />}/>
             <Route path ='/shodo' element={<Shodo />}/>
+            <Route path='/contact' element={<Contact />} />
             <Route path ='*' element={<Navigate to ='/' />}/>
           </Routes>
       </main>
