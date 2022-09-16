514787209df131fc3cc4afe9847d937bd309d728
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-16 22:36:24 +0900

    adds header to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,10 +3,14 @@ import React from 'react';
 function Home() {
 	return (
 		<>
-        <video autoplay="autoplay" loop="true" muted defaultmuted playsinline class="absolute z-10 w-auto min-w-full min-h-full max-w-none" src="https://assets.mixkit.co/videos/preview/mixkit-waterfall-in-woods-2212-large.mp4">
+        <video id="home-video" autoplay="autoplay" loop="true" muted defaultmuted playsinline src="https://assets.mixkit.co/videos/preview/mixkit-waterfall-in-woods-2212-large.mp4">
     		{/* <source src="https://assets.mixkit.co/videos/preview/mixkit-waterfall-in-woods-2212-large.mp4" type="video/mp4"/> */}
   		</video>
-		
+		  <header id="viewport-header">
+			<h1 id="viewport-h1">
+				CROSS MEDIA <br /> JAPAN
+			</h1>
+		</header>
         </>
 	);
 };
