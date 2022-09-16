894f7695501cf955cc218d8351d92b3d7e356a80
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-16 20:46:35 +0900

    restyles navbar with react-bootstrap

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,41 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
 
 function Navigation() {
 	return (
 		<div>
-			<nav class="bg-white border-gray-200 px-2">
-				<div class="container mx-auto flex flex-wrap items-center justify-between">
-					<Link to="/" class="flex">
-					<svg class="h-10 mr-3" width="51" height="70" viewBox="0 0 51 70" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path d="M1 53H27.9022C40.6587 53 51 42.7025 51 30H24.0978C11.3412 30 1 40.2975 1 53Z" fill="#76A9FA"></path><path d="M-0.876544 32.1644L-0.876544 66.411C11.9849 66.411 22.4111 55.9847 22.4111 43.1233L22.4111 8.87674C10.1196 8.98051 0.518714 19.5571 -0.876544 32.1644Z" fill="#A4CAFE"></path><path d="M50 5H23.0978C10.3413 5 0 15.2975 0 28H26.9022C39.6588 28 50 17.7025 50 5Z" fill="#1C64F2"></path></g><defs><clipPath id="clip0"><rect width="51" height="70" fill="white"></rect></clipPath></defs></svg>
-						<span class="self-center text-lg font-semibold whitespace-nowrap">Cross Media</span>
-					</Link>
-					<button data-collapse-toggle="mobile-menu" type="button" class="md:hidden ml-3 text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center" aria-controls="mobile-menu-2" aria-expanded="false">
-					<span class="sr-only">Open main menu</span>
-					<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-					<svg class="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-					</button>
-					<div class="hidden md:block w-full md:w-auto" id="mobile-menu">
-					<ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
-						<li>
-						<Link to="/" class="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded" aria-current="page">Home</Link>
-						</li>
-						<li>
-						<Link to="/about" class="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">About</Link>
-						</li>
-						<li>
-						<Link to="/shodo" class="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Shodo</Link>
-						</li>
-						<li>
-						<Link to="/bonsai" class="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Bonsai</Link>
-						</li>
-						<li>
-						<Link to="/contact" class="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Contact</Link>
-						</li>
-					</ul>
-					</div>
-				</div>
-			</nav>
+			<Navbar bg="dark" variant="dark" expand="lg">
+			<Container>
+				<Navbar.Brand as={Link} to='/'>
+					<img
+					alt=""
+					src="/logo.svg"
+					width="30"
+					height="30"
+					className="d-inline-block align-top"
+					/>{' '}
+					CROSS MEDIA
+			</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<Nav.Link as={Link} to='/'>HOME</Nav.Link>
+						<Nav.Link as={Link} to='/about'>ABOUT</Nav.Link>
+						<NavDropdown title="SERVICES" id="basic-nav-dropdown">
+							<NavDropdown.Item as={Link} to='/shodo'>
+								SHODO
+							</NavDropdown.Item>
+							<NavDropdown.Item as={Link} to ='/bonsai'>
+								BONSAI
+							</NavDropdown.Item>
+						</NavDropdown>
+					</Nav>
+					<Nav>
+						<Nav.Link as={Link} to='/contact'>CONTACT</Nav.Link>
+					</Nav>
+					</Navbar.Collapse>
+				</Container>
+    		</Navbar>
 		</div>
 	)
 };
