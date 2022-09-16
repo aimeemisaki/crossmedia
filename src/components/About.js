0c4a567e57b42ee4a3490c4d48df7c5cb02b9c0d
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-16 23:44:04 +0900

    creates About.css and adds background video to About.js component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './styles/About.css';
 
 function About() {
 	return (
 		<>
-		<p> Hello!!!</p>
+		<video id="about-video" autoplay="autoplay" loop="true" muted defaultmuted playsinline src="https://assets.mixkit.co/videos/preview/mixkit-forest-covered-by-mist-at-sunrise-from-the-heights-28339-large.mp4">
+  		</video>
         </>
 	);
 };
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,31 +9,31 @@ function Navigation() {
 			<Navbar bg="dark" variant="dark" expand="lg">
 			<Container>
 				<Navbar.Brand as={Link} to='/' id="nav-bar-header">
-					<img
+					{/* <img
 					alt=""
 					src="/logo.svg"
 					width="30"
 					height="30"
 					className="d-inline-block align-top"
-					/>{' '}
+					/>{' '} */}
 					<h2 id="nav-bar-header">CROSS MEDIA</h2>
 			</Navbar.Brand>
 					<Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link as={Link} to='/'>HOME</Nav.Link>
-						<Nav.Link as={Link} to='/about'>ABOUT</Nav.Link>
-						<NavDropdown title="SERVICES" id="basic-nav-dropdown">
-							<NavDropdown.Item as={Link} to='/shodo'>
+						<Nav.Link as={Link} to='/' class="nav-text">HOME</Nav.Link>
+						<Nav.Link as={Link} to='/about' class="nav-text">ABOUT</Nav.Link>
+						<NavDropdown title="SERVICES" id="basic-nav-dropdown" class="nav-text">
+							<NavDropdown.Item as={Link} to='/shodo' class="nav-text">
 								SHODO
 							</NavDropdown.Item>
-							<NavDropdown.Item as={Link} to ='/bonsai'>
+							<NavDropdown.Item as={Link} to ='/bonsai' class="nav-text">
 								BONSAI
 							</NavDropdown.Item>
 						</NavDropdown>
 					</Nav>
 					<Nav>
-						<Nav.Link as={Link} to='/contact'>CONTACT</Nav.Link>
+						<Nav.Link as={Link} to='/contact' id="nav-text">CONTACT</Nav.Link>
 					</Nav>
 					</Navbar.Collapse>
 				</Container>
