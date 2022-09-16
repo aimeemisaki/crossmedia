faa0e37a22e79fef42f7c61fc47455e03cfcc362
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-17 00:33:07 +0900

    builds About.js content and completes styling in About.css

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './styles/About.css';
+import { Card } from 'react-bootstrap'
 
 function About() {
 	return (
 		<>
 		<video id="about-video" autoplay="autoplay" loop="true" muted defaultmuted playsinline src="https://assets.mixkit.co/videos/preview/mixkit-forest-covered-by-mist-at-sunrise-from-the-heights-28339-large.mp4">
   		</video>
+		  <div class="grid place-items-center h-screen">
+		<Card id="viewport-header">
+		  <header>
+				<h1 id='about-header'>
+					ABOUT US
+				</h1>
+				<h2 class="about-text">At Cross Media, we are dedicated to spreading awareness of the beauties of Japan's history to people from around the world. Our goal is to teach global visitors two of the most important foundations of ancient and present-day Japanese culture, such as <i>Shodo</i> (書道) and <i>Bonsai</i> (盆栽).</h2>
+		</header>
+		</Card>
+		</div>
         </>
 	);
 };
