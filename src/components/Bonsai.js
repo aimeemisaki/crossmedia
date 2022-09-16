53637f7d6d4182ea67ed3a462c67888712ac6055
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-16 21:18:09 +0900

    builds bonsai page with filler images

--- src/components/Bonsai.js
+++ src/components/Bonsai.js
@@ -1,8 +1,112 @@
 import React from 'react';
 
+
 function Bonsai() {
+
+	// function modalHandler (event) {
+	// 	 event.target.visibility = visible
+	// }
 	return (
 		<>
+		<div class="grid grid-cols-4 gap-4 mx-3 my-3">
+			<div>
+				<figure class="relative max-w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+					<img 
+					class="rounded-lg" 
+					src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" 
+					alt="naomi poster"
+					/>
+					<figcaption class="absolute bottom-6 px-4 text-lg text-center text-transparent md:text-white">
+						<p>Feature on Naomi (2022)</p>
+					</figcaption>
+				</figure>
+			</div>
+			<div>
+				<figure class="relative max-w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+					<img 
+					class="rounded-lg" 
+					src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" 
+					alt="naomi poster"
+					/>
+					<figcaption class="absolute bottom-6 px-4 text-lg text-center text-transparent md:text-white">
+						<p>Feature on Naomi (2022)</p>
+					</figcaption>
+				</figure>
+			</div>
+			<div>
+				<figure class="relative max-w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+					<img 
+					class="rounded-lg" 
+					src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" 
+					alt="naomi poster"
+					/>
+					<figcaption class="absolute bottom-6 px-4 text-lg text-center text-transparent md:text-white">
+						<p>Feature on Naomi (2022)</p>
+					</figcaption>
+				</figure>
+			</div>
+			<div>
+				<figure class="relative max-w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+					<img 
+					class="rounded-lg" 
+					src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" 
+					alt="naomi poster"
+					/>
+					<figcaption class="absolute bottom-6 px-4 text-lg text-center text-transparent md:text-white">
+						<p>Feature on Naomi (2022)</p>
+					</figcaption>
+				</figure>
+			</div>
+			<div>
+				<figure class="relative max-w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+					<img 
+					class="rounded-lg" 
+					src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" 
+					alt="naomi poster"
+					/>
+					<figcaption class="absolute bottom-6 px-4 text-lg text-center text-transparent md:text-white">
+						<p>Feature on Naomi (2022)</p>
+					</figcaption>
+				</figure>
+			</div>
+			<div>
+				<figure class="relative max-w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+					<img 
+					class="rounded-lg" 
+					src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" 
+					alt="naomi poster"
+					/>
+					<figcaption class="absolute bottom-6 px-4 text-lg text-center text-transparent md:text-white">
+						<p>Feature on Naomi (2022)</p>
+					</figcaption>
+				</figure>
+			</div>
+			<div>
+				<figure class="relative max-w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+					<img 
+					class="rounded-lg" 
+					src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" 
+					alt="naomi poster"
+					/>
+					<figcaption class="absolute bottom-6 px-4 text-lg text-center text-transparent md:text-white">
+						<p>Feature on Naomi (2022)</p>
+					</figcaption>
+				</figure>
+			</div>
+			<div>
+				<figure class="relative max-w-md transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0">
+					<img 
+					class="rounded-lg" 
+					src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/content-gallery-3.png" 
+					alt="naomi poster"
+					/>
+					<figcaption class="absolute bottom-6 px-4 text-lg text-center text-transparent md:text-white">
+						<p>Feature on Naomi (2022)</p>
+					</figcaption>
+				</figure>
+			</div>
+			
+		</div>
         </>
 	);
 };
