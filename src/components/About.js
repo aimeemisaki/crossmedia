91235258093898ed5e428ba3b2f636ffd7094ea7
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-17 01:08:04 +0900

    builds about page and styles

--- src/components/About.js
+++ src/components/About.js
@@ -8,12 +8,13 @@ function About() {
 		<video id="about-video" autoplay="autoplay" loop="true" muted defaultmuted playsinline src="https://assets.mixkit.co/videos/preview/mixkit-forest-covered-by-mist-at-sunrise-from-the-heights-28339-large.mp4">
   		</video>
 		  <div class="grid place-items-center h-screen">
-		<Card id="viewport-header">
+		<Card id="about-container">
 		  <header>
-				<h1 id='about-header'>
+				<h1 id='about-header' class="pb-3 md:pb-14">
 					ABOUT US
 				</h1>
-				<h2 class="about-text">At Cross Media, we are dedicated to spreading awareness of the beauties of Japan's history to people from around the world. Our goal is to teach global visitors two of the most important foundations of ancient and present-day Japanese culture, such as <i>Shodo</i> (書道) and <i>Bonsai</i> (盆栽).</h2>
+				<h2 class="about-text">At <span class="cross-media">Cross Media</span>, we are dedicated to spreading awareness of the beauties of Japan's history to people from around the world. Our goal is to teach global visitors two of the most important foundations of ancient and present-day Japanese culture: <br/>
+				<i>Shodo</i> (書道) and <i>Bonsai</i> (盆栽).</h2>
 		</header>
 		</Card>
 		</div>
