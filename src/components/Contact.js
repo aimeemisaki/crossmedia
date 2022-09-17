5493f2df211d8422b371b20566c992c9bad09c84
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-17 16:28:02 +0900

    imports koi.mp4 video and adds the video to contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import koi from './media/koi.mp4';
 
 function Contact() {
 	return (
 		<>
-            
+		<video id="contact-video" autoplay="autoplay" loop="true" muted defaultmuted playsinline src={koi}>
+  		</video>
         </>
 	);
 };
