d566cd5bc46a58dfd1c887b1fcbc6fb0f768defb
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-17 17:08:04 +0900

    styles Contact page in Contact.css and Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import koi from './media/koi.mp4';
+import './styles/Contact.css'
 
 function Contact() {
 	return (
@@ -7,18 +8,17 @@ function Contact() {
 		<video id="contact-video" autoplay="autoplay" loop="true" muted defaultmuted playsinline src={koi}>
   		</video>
 		<div class="grid place-items-center h-screen">
-			<div id="contact-container" class="rounded-md h-96 w-64 md:h-64 md:w-96">
+			<div id="contact-container" class="rounded-md h-112 w-80  md:w-160">
 				<header>
 					<h1 id='contact-header' class="pb-3 md:pb-14">
 						CONTACT US
 					</h1>
-					<h2 class="about-text">
-						crossmedia@gmail.com <br/> 
-						〒153-0064 東京都目黒区下目黒5-35-3 <br/>
-						〒153-0064 5-35-3 Shimomeguro, Meguro-ku, Tokyo<br/>
-						080-9532-8154
+					<h2 class="about-text leading-7">
+						〒153-0064 5-35-3 Shimomeguro, Meguro-ku, Tokyo, JAPAN<br/>
+						080-9532-8154<br/>
+						crossmedia@gmail.com 
 					</h2>
-					<h1 id='contact-header'class="pb-3 md:pb-14">
+					<h1 id='subscribe-header'class="pb-3 md:pb-14">
 						SUBSCRIBE TO OUR NEWSLETTER
 					</h1>
 					<form
@@ -27,11 +27,16 @@ function Contact() {
 					enctype="multipart/form-data"
 					>
 						<label class="block mb-6">
-							<span class="text-gray-300">EMAIL ADDRESS</span>
+							<span 
+							class=
+							"contact-text text-gray-300">
+								EMAIL ADDRESS
+							</span>
 							<input
 							name="email"
 							type="email"
-							class="
+							class=" 
+								contact-text
 								block
 								w-full
 								mt-1
@@ -43,16 +48,17 @@ function Contact() {
 								focus:ring-indigo-200
 								focus:ring-opacity-50
 								bg-transparent
-								placeholder-gray-600
-								text-gray-300
+								placeholder-gray-400
+								text-gray-100
 							"
 							placeholder="joe.smith@example.com"
 							/>
 						</label>
-						<div class="mb-6">
+						<div class="content-center">
 							<button
 							type="submit"
 							class="
+								contact-header
 								h-10
 								px-5
 								text-slate-50
